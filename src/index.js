14437a71e76e3594c3749f14442158e272da0da0
ParekhKunal <parekhkunal09@gmail.com>
2026-06-15 08:06:28 +0530

    ECS task

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello, World!' });
 });
 
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK' });
+});
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
